2f4077bba07a4813d0fabb278ac8e6ad61f82754
AGVF-22 <amanda.gabrielaccb@gmail.com>
2021-02-14 19:29:15 -0300

    Implementar categoria

---
+++ application/views/nova_categoria.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar categoria</title>
+</head>
+<body>
+
+<h2>Cadastrar categoria</h2>
+    
+    <label for="data">Nome: <input type="text" name="nome"></label><br>
+    <label for="descricao">Descrição: <input type="text" name="descricao"></label><br>
+    <button>Enviar</button>
+    <button>Apagar</button>
+
+</body>
+</html>

caf8913a1fbbe8772fbb77f3464b9f4b28aba721
AGVF-22 <amanda.gabrielaccb@gmail.com>
2021-02-14 18:51:51 -0300

    Alteracoes

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de gastos</title>
+</head>
+<body>
+
+<table>
+
+    <tr>
+        <td>Data</td>
+        <td>Descricao</td>
+        <td>Valor</td>
+    </tr>
+    <tr>
+        <td>14/02/2021</td>
+        <td>Passagem de ônibus</td>
+        <td>R$ 2,00</td>
+    </tr>
+    <tr>
+        <td>14/02/2021</td>
+        <td>Salgado</td>
+        <td>R$ 2,50</td>
+    </tr>
+    <tr>
+        <td>14/02/2021</td>
+        <td>Refrigerante</td>
+        <td>R$ 3,00</td>
+    </tr>
+
+</table>
+    
+</body>
+</html>
